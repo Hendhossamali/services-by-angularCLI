09e23a0be7f9d482ffb76456d3fd548245f015a6
hend <hendhossam36@gmail.com>
2023-12-20 16:10:35 +0200

    create-service-for-products-and-cart

---
+++ src/app/cart-page/cart-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Cart } from '../shared/models/Cart';
+import { CartService } from '../services/cart/cart.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cart-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cart-page.component.html',
+  styleUrl: './cart-page.component.css'
+})
+export class CartPageComponent {
+  cart!: Cart;
+
+  constructor(private cartService:CartService){
+    this.setCart();
+  }
+  setCart(){
+    this.cart=this.cartService.getCart();
+  }
+}
---
+++ src/app/services/cart/cart.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { Cart } from "../../shared/models/Cart";
+import { Product } from "../../shared/models/Product";
+import CartItem from "../../shared/models/CartItem";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CartService {
+    private cart: Cart = new Cart();
+
+    constructor() { }
+    // Add new Product to cart 
+    addtoCart(product: Product): void {
+        this.cart.items.push(new CartItem(product));
+        // console.log(this.cart);
+    }
+    // Display Cart elements (Items)
+    getCart(): Cart{
+        return this.cart;
+    }
+}
